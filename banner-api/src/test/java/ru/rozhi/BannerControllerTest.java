56477906ee6796053b573ed13a6d65bbf6e66555
nastyarozhina <nastarozina221@gmail.com>
2026-09-12 02:31:25 +0300

    rufrhfufhfufhfhffjd

--- banner-api/src/test/java/ru/rozhi/BannerControllerTest.java
+++ banner-api/src/test/java/ru/rozhi/BannerControllerTest.java
@@ -12,11 +12,14 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.mongodb.MongoDBContainer;
+import tools.jackson.core.type.TypeReference;
 import org.testcontainers.utility.DockerImageName;
 import ru.rozhi.controller.dto.BannerResponse;
 import ru.rozhi.repository.BannerRepository;
 import ru.rozhi.repository.model.Banner;
 import tools.jackson.databind.ObjectMapper;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -49,17 +52,43 @@ public class BannerControllerTest {
 
     @Test
     @SneakyThrows
-    void shouldReturnFirstBanner() {
+    void shouldReturnAllBanners() {
         bannerRepository.save(Banner.builder().name("NAME1").description("DESCRIPTION1").build());
         bannerRepository.save(Banner.builder().name("NAME2").description("DESCRIPTION2").build());
-        BannerResponse expected = new BannerResponse("NAME1", "DESCRIPTION1");
+        List<BannerResponse> expected = List.of(new BannerResponse("NAME1", "DESCRIPTION1"),
+                new BannerResponse("NAME2", "DESCRIPTION2"));
 
         MvcResult result = mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andReturn();
 
-        BannerResponse response = objectMapper.readValue(result.getResponse().getContentAsString(), BannerResponse.class);
+        List<BannerResponse> response =
+                objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {});
 
         assertThat(response).isEqualTo(expected);
+    }
+
+    @Test
+    @SneakyThrows
+    void shouldReturnBannerById() {
+        bannerRepository.save(Banner.builder().id("banner1").name("NAME1").description("DESCRIPTION1").build());
+        bannerRepository.save(Banner.builder().id("banner2").name("NAME2").description("DESCRIPTION2").build());
+        BannerResponse expected = new BannerResponse("NAME2", "DESCRIPTION2");
+
+        MvcResult result = mockMvc.perform(get("/banner2"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        BannerResponse response =
+                objectMapper.readValue(result.getResponse().getContentAsString(), BannerResponse.class);
+
+        assertThat(response).isEqualTo(expected);
+    }
+
+    @Test
+    @SneakyThrows
+    void shouldReturn404WhenBannerNotFound() {
+        bannerRepository.save(Banner.builder().id("banner1").name("NAME1").description("DESCRIPTION1").build());
+        mockMvc.perform(get("/banner2")).andExpect(status().isNotFound());
     }
 }
