877538afc3b1d9461b6063abe1f36ed5bca58d05
nastyarozhina <nastarozina221@gmail.com>
2026-09-19 18:37:31 +0300

    add smth

--- category-api/src/main/java/ru/rozhi/controller/CategoryController.java
+++ category-api/src/main/java/ru/rozhi/controller/CategoryController.java
@@ -1,6 +1,10 @@
 package ru.rozhi.controller;
 
+import org.jspecify.annotations.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import ru.rozhi.service.CategoryService;
 
@@ -9,5 +13,8 @@ public class CategoryController {
 
     @Autowired
     private CategoryService categoryService;
+
+    @PostMapping
+    public void createBanner() {}
 
 }
